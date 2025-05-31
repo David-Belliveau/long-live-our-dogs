6cdcb36cc885b6fc47d2fb5a6e1b3b02bd86b08e
David Belliveau <dibsy@mac.mynetworksettings.com>
2025-05-31 12:07:51 -0400

    chore: recreate virtual environment and set up Gunicorn for deployment

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -7,9 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# application = get_wsgi_application()
+
+import sys
+import os
 from django.core.wsgi import get_wsgi_application
+
+# Add the backend directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
